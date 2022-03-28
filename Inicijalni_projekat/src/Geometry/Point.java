4c61a87a5b33a96a48088115b6b6bf398fd27d63
it3-2021 <it3-2021@AR4-07.mshome.net>
2022-03-28 09:19:37 +0200

    VEZBA 4

--- Inicijalni_projekat/src/Geometry/Point.java
+++ Inicijalni_projekat/src/Geometry/Point.java
@@ -34,15 +34,9 @@ public class Point {
 	}
 
 
-
-
-
-
 	public boolean isSelected() {
 		return selected;
 	}
-
-
 
 
 
@@ -54,18 +48,58 @@ public class Point {
 
 
 
-
-
 	public int getY() {
 		return y;
 	}
 
 
+	//vezba 4
 
-
+	public Point () {};
+	public Point (int x, int y)
+	{
+		this.x=x;
+		this.y=y;    //potpis metode(ime, redosled i broj tipova podataka) ne smeju biti isti, overloading-preklapanje naziva
+		//vise metpda koje se isto nazivaju
+	}
+	public Point (int x, int y, boolean selected) 
+	{//this.x=x;
+	//this.y=y; 
+		this(x,y);
+	this.selected=selected;	
+	
+	};
 
 
 	
+	/*objct je roditeljska klasa, sve ostale nasledjuju 
+	 * njene osobine 
+	 * r1=r2    prenosi se i vrednost i referenca
+	 * r1==r2 porede se reference, i imace iste vrednosti,uvek true
+	 * ako imamo new onda su dva objekta, dve raylicite refenrece
+	 * equals dal su jednake vrednosti
+	 * overwriting predefinisanje metode
+	 */
+	
+	public boolean equals(Object obj) {
+		if (obj instanceof Point) {
+			Point pomocna = (Point) obj;
+			/*downcast objekat pretvaramo u tacku, konvertivanje*/
+			if (this.x == pomocna.x && this.y == pomocna.y)
+				return true;
+			else
+				return false;
+		} else
+			return false;
+	}
+
+
+
+	public String toString() {
+		return "(" + x + "," + y + ")";
+		//return x; ne moze, konkatenacija
+	}
+
 	
 	
 }
--- Inicijalni_projekat/src/Geometry/Test.java
+++ Inicijalni_projekat/src/Geometry/Test.java
@@ -48,7 +48,7 @@ l1.getEndPoint().setX((int)(l1.length()-(l1.getStartPoint().getX()+l1.getStartPo
 
 
 //r1.setUpperLeftPoint(new Point(p.getX(),p.getY()))
-r1.getUpperLeftPoint().setX(10);
+                             
 r1.setUpperLeftPoint(p);
 r1.getUpperLeftPoint().setX(10);
 r1.getUpperLeftPoint().setY(15);
@@ -59,6 +59,44 @@ r1.setWidth(20);
 c1.getCenter().setX(r1.area()-l1.getStartPoint().getY());
 System.out.println(c1.getCenter().getX());
 
+//Vezbe 4
+		/*
+		 * 1.Postaviti koordinatu x centra ranije kreiranog kruga k na vrednost zbira
+		 * vrednosti poluprecnika  kruga k i vrednosti koja predstavlja udaljenost
+		 * pocetne i krajnje tacke prethodno kreirane linije lin (NE duzine). Sve
+		 * objekte kreirati samostalno.
+		 */
 
+c1.getCenter().setX(c1.getRadius()+(int)(l1.getStartPoint().distance(l1.getEndPoint().getX(), l1.getEndPoint().getY())));
+
+/* Point p=new Point(); d0,0, false
+ * public void, ne pise se tip podatka , Point (){};
+ */
+
+
+Point p2=new Point(5,5);
+
+System.out.println(p2.getX());
+
+Point p3=new Point (5,5, false);
+System.out.println(p3.isSelected());
+
+System.out.println(p3);
+System.out.println(l1);
+
+
+Point p4=new Point(10,15);
+Point p5=new Point(15,20);
+System.out.println(p4.equals(p5));
+System.out.println(p4==p5);
+
+p4=p5;
+System.out.println(p4.equals(p5));
+System.out.println(p4==p5);
+
+Point p6=new Point(10,15);
+Point p7=new Point(10,15);
+System.out.println(p6==p7);
+System.out.println(p6.equals(p7));
 	}
 }
